75466a83e6b49a12e7d53db43789ae8d4cf8ccd5
Kyrylo Bulyk <kyrylo.bulyk@gmail.com>
2024-10-10 00:26:43 +0200

    Delete Ukrainian comments and create .gitignore file

--- src/main.c
+++ src/main.c
@@ -2,11 +2,10 @@
 #include "socket.h"
 
 int main() {
-    int port = 8080; // Вибраний порт для сервера
+    int port = 8080;
     int server_fd = create_tcp_server(port);
     
     printf("HTTPS Server is running and waiting for connections...\n");
     
-    // Тут будемо обробляти підключення клієнтів (пізніше)
     return 0;
 }
--- src/socket.c
+++ src/socket.c
@@ -10,36 +10,34 @@ int create_tcp_server(int port) {
     struct sockaddr_in address;
     int opt = 1;
 
-    // Створення сокета
+    // Create of Socket
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Не вдалось створити сокет");
+        perror("Unable to connect to the socket");
         exit(EXIT_FAILURE);
     }
 
-    // Налаштування параметрів сокета
-    // Заміна старого рядка setsockopt
+    // Setting parameters of socket
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        perror("Помилка налаштування сокета");
+        perror("Error in setting parameter of socket");
         exit(EXIT_FAILURE);
     }
-
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
 
-    // Прив'язка сокета до порту
+    // Socker binding to port
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Помилка прив'язки сокета");
+        perror("Error in socker binding");
         exit(EXIT_FAILURE);
     }
 
-    // Початок прослуховування на сокеті
+    // Start of listening on socket
     if (listen(server_fd, 3) < 0) {
-        perror("Помилка прослуховування");
+        perror("Error in listeting");
         exit(EXIT_FAILURE);
     }
 
-    printf("Сервер запущено на порту %d\n", port);
+    printf("Server was started on port %d\n", port);
     return server_fd;
 }
